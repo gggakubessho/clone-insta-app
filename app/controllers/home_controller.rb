7a487c38c396a84fb3dee03e073f05bb8f051904
GakuBessho <ec2-user@ip-172-31-41-224.ap-northeast-1.compute.internal>
2020-03-15 14:42:39 +0000

    add top-page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,13 @@
 
 class HomeController < ApplicationController
   before_action :authenticate_user!, only: :show
-  def index; end
+  def index
+    if user_signed_in?
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    else
+      redirect_to new_user_registration_path
+    end
+  end
 
   def show; end
 end
